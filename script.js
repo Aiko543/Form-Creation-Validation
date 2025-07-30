6425a56f278bb3b5beb5d914b3219bd89dd0db45
Aiko543 <njokistephanie01@gmail.com>
2025-07-30 07:52:43 +0300

    Deep Dive into Javascript

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ document.addEventListener('DOMContentLoaded', () => {
         // Displaying feedback
         feedbackDiv.style.display = 'block';
         if (isValid) {
-            feedbackDiv.textContent = 'Registration successful!';
-            feedbackDiv.style.color = '#28a745'; // Green color for success
+            feedbackDiv.textContent = "Registration successful!";
+            feedbackDiv.style.color = "#28a745"; // Green color for success
         } else {
             feedbackDiv.textContent = messages.join('<br>');
-            feedbackDiv.style.color = '#dc3545'; // Red color for error
+            feedbackDiv.style.color = "#dc3545"; // Red color for error
         }
     });
 });
